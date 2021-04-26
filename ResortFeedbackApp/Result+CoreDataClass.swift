7ac39f8e4dc57391a1274aae095658d83024e473
Kirtland Ross <kirtlandross@gmail.com>
2021-04-26 16:05:22 -0400

    Assets upgrade

---
+++ ResortFeedbackApp/Result+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Result+CoreDataClass.swift
+//  ResortFeedbackApp
+//
+//  Created by Kirtland Ross on 4/26/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Result)
+public class Result: NSManagedObject {
+
+}
