863a1e0e95bd4c46965313a45f85cc75559d2580
Kirtland Ross <kirtlandross@gmail.com>
2021-04-28 17:41:20 -0400

    conditional segues figured out

---
+++ ResortFeedbackApp/ResortFeedbackApp/NavigationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NavigationViewController.swift
+//  ResortFeedbackApp
+//
+//  Created by Kirtland Ross on 4/28/21.
+//
+
+import UIKit
+
+class NavigationViewController: UINavigationController {
+    var user : User?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
