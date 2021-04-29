9177fec2dbca2dd031c7f152183adc2328ff98bc
Kirtland Ross <kirtlandross@gmail.com>
2021-04-29 15:30:38 -0400

    remember me switch working

--- ResortFeedbackApp/ResortFeedbackApp/LoginViewController.swift
+++ ResortFeedbackApp/ResortFeedbackApp/LoginViewController.swift
@@ -10,18 +10,28 @@ import UIKit
 class LoginViewController: UIViewController {
     var userDictionary : [String: User]?
     var dbhelp = DBHelper()
-
+    let ud = UserDefaults.standard
+    private var user : User?
 
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var username: UITextField!
 
-    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var rememberMe: UISwitch!
+    @IBOutlet weak var userErr: UILabel!
+    @IBOutlet weak var passErr: UILabel!
     @IBOutlet weak var backgoundY: NSLayoutConstraint!
     @IBOutlet weak var backgroundImage: UIImageView!
     let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
     override func viewDidLoad() {
         super.viewDidLoad()
-        errorLabel.alpha = 0
+        passErr.alpha = 0
+        userErr.alpha = 0
+        username.text = ud.string(forKey: "id")
+        password.text = ud.string(forKey: "pass")
+        if ud.bool(forKey: "bool"){
+            rememberMe.isOn = true
+        }
+
     }
     override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
         switch UIDevice.current.orientation{
@@ -58,17 +68,44 @@ class LoginViewController: UIViewController {
     }
 
     @IBAction func signIn(_ sender: Any) {
-        let user = dbhelp.getOneData(name: username.text!)
-        if user.passCheck(password.text!){
+        user = dbhelp.getOneData(name: username.text!)
+        if user!.name == nil{
+            userErr.alpha = 1
+        }
+        else if user!.passCheck(password.text!){
             (self.navigationController as! NavigationViewController).user = user
             self.performSegue(withIdentifier: "MainMenu", sender: self)
-            errorLabel.alpha = 0
+            passErr.alpha = 0
+            userErr.alpha = 0
+            if !rememberMe.isOn{
+                username.text = ""
+                password.text = ""
+            }
         }
         else{
-            errorLabel.alpha = 1
+            passErr.alpha = 1
         }
         
     }
   
+    @IBAction func rememberMe(_ sender: UISwitch) {
+        if sender.isOn{
+
+            ud.set(username.text, forKey: "id")
+            ud.set(password.text, forKey: "pass")
+
+
+            print("saved!")
+
+        }
+        else{
+            ud.removeObject(forKey: "id")
+            ud.removeObject(forKey: "pass")
+            print("unsaved!")
+        }
+        ud.set(rememberMe.isOn, forKey: "bool")
+
+    }
 }
+
 
