e29ac9acd07e3d639bf2a79ec971befa064af0e1
Kirtland Ross <kirtlandross@gmail.com>
2021-05-04 17:12:17 -0400

    Animated splash screen implemented

--- ResortFeedbackApp/ResortFeedbackApp/SpashScreenViewController.swift
+++ ResortFeedbackApp/ResortFeedbackApp/SpashScreenViewController.swift
@@ -9,16 +9,33 @@ import UIKit
 
 class SpashScreenViewController: UIViewController {
 
-    @IBOutlet weak var logo: UIImageView!
+
+    @IBOutlet weak var logoX: NSLayoutConstraint!
+    @IBOutlet weak var logoY: NSLayoutConstraint!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         _ = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(segue), userInfo: nil, repeats: false)
-
+        
     }
-    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+
+        UIView.animate(withDuration: 0.2, delay: 1.5, options: UIView.AnimationOptions.curveLinear, animations: {
+              self.logoX.constant += self.view.bounds.width
+              self.view.layoutIfNeeded()
+          }, completion: nil)
+    }
 
     @objc func segue(){
         performSegue(withIdentifier: "splash", sender: self)
     }
+
 
 }
