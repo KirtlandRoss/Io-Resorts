f14a26664c84a4fb80902f58d1d7bf8d56c42114
Kirtland Ross <kirtlandross@gmail.com>
2021-05-05 15:43:44 -0400

    splash screen logo was over-constrained and no longer animating. Animation fixed.

--- ResortFeedbackApp/ResortFeedbackApp/SpashScreenViewController.swift
+++ ResortFeedbackApp/ResortFeedbackApp/SpashScreenViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 class SpashScreenViewController: UIViewController {
 
 
+
     @IBOutlet weak var logoX: NSLayoutConstraint!
-    @IBOutlet weak var logoY: NSLayoutConstraint!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,13 +22,9 @@ class SpashScreenViewController: UIViewController {
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-
-        
     }
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-
-
         UIView.animate(withDuration: 0.2, delay: 1.5, options: UIView.AnimationOptions.curveLinear, animations: {
               self.logoX.constant += self.view.bounds.width
               self.view.layoutIfNeeded()
