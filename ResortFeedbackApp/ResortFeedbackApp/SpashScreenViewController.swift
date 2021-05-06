d8ecbee7dce4b38ac23a3cce4f642209bd170164
Kirtland Ross <kirtlandross@gmail.com>
2021-05-05 20:08:16 -0400

    Second iteration of cool launch and splash animation

--- ResortFeedbackApp/ResortFeedbackApp/SpashScreenViewController.swift
+++ ResortFeedbackApp/ResortFeedbackApp/SpashScreenViewController.swift
@@ -8,34 +8,41 @@
 import UIKit
 
 class SpashScreenViewController: UIViewController {
-
-
-
-
+    
+    
+    
+    
     @IBOutlet weak var circleY: NSLayoutConstraint!
-
-
+    @IBOutlet weak var fullLogo: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         overrideUserInterfaceStyle = .dark 
         _ = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(segue), userInfo: nil, repeats: false)
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
-
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        UIView.animate(withDuration: 0.5, delay: 1, options: UIView.AnimationOptions.curveEaseOut, animations: {
-              self.circleY.constant -= 200
-              self.view.layoutIfNeeded()
-          }, completion: nil)
+        UIView.animate(withDuration: 0.5, delay: 0.5, options: UIView.AnimationOptions.curveEaseOut, animations: {
+            self.circleY.constant -= 200
+            
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+        UIView.animate(withDuration: 0.5, delay: 1.5, options: UIView.AnimationOptions.curveEaseOut, animations: {
+            self.fullLogo.alpha = 1
+            
+            self.view.layoutIfNeeded()
+        }, completion: nil)
     }
-
+    
     @objc func segue(){
         performSegue(withIdentifier: "splash", sender: self)
     }
-
-
+    
+    
 }
